b99da9c07194954be7fdf5442663088a89453424
Leonardo <devleonardorabelo@gmail.com>
2021-01-02 19:36:33 -0300

    chore: added cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connect } from 'mongoose';
+import cors from 'cors';
 import { AuthRoute, ProductRoute, PaymentRoute } from './routes';
 
 dotenv.config();
 const app = express();
 
 connect(process.env.BD, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(cors());
 app.use(express.json());
 app.use(AuthRoute, ProductRoute, PaymentRoute);
 
